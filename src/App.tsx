74b44c3ef61f484ea1e1e757bbf26705b7926b4d
Abhishek Patil <patilabhishek394@gmail.com>
2025-03-16 11:02:39 +0530

    add navbar design in the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-cyan-500">Hello world!</h1>
+    <div>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline text-cyan-500">
+        Hello world!
+      </h1>
+    </div>
   );
 }
